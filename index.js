13b567e93c93a547177f827b5db44aa06e3be4fd
Vinod Tysoe <82728229+Vizzoid@users.noreply.github.com>
2024-02-04 22:33:54 +0000

    implementation of player control, bug: player is stuck if surrounded by bots

--- index.js
+++ index.js
@@ -32,12 +32,25 @@ class Board {
      * the graphical display, the index is built by adding the y value, flipped, and 
      * multiplied by 5, adding the x value to return the accurate array index.
      *
-     * @param {*} x integer x coordinate
-     * @param {*} y integer y coordinate
-     * @returns {*} integer raw index of position in board.array
+     * @param {number} x integer x coordinate
+     * @param {number} y integer y coordinate
+     * @returns {number} integer raw index of position in board.array
      */
     toRawIndex(x, y) {
         return (4 - y) * 5 + x;
+    }
+
+    /**
+     * x and y integer tile positions on board from raw index
+     * 
+     * @param {number} index 
+     * @generator
+     * @yields {number} x and y integer tile positions on board
+     */
+    *fromRawIndex(index) {
+        const x = index % 5;
+        yield x;
+        yield -((index - x) / 5 - 4);
     }
 
     /**
@@ -126,11 +139,7 @@ class Board {
 
     /**
      * Adds the robot of the inputted size to a random location on the board that 
-     * does not have a robot already. This function works by going through every index of the
-     * board array and building an indices array of empty board locations with their raw 
-     * index. If this indices array is empty, the internal board array is 
-     * [reset()]{@link Board#reset}. Otherwise, this robot is added to a random location in
-     * the indices array.
+     * does not have a robot already. Using [randomEmptyPos()]{@link Board#randomEmptyPos}
      * 
      * The inside of the Board class uses an array for the robots which is one dimensional.
      * This array has equal columns and rows. To make the visual code representation match 
@@ -140,21 +149,71 @@ class Board {
      * @param {number} size the size of the robot to be added (1-4) inclusive
      */
     randomRobot(size) {
+        this.setRobotRaw(this.randomEmptyPos(), size);
+    }
+
+    /**
+     * Gives a random location on the board that does not have a robot already. This 
+     * function works by going through every index of the board array and building an 
+     * indices array of empty board locations with their raw index. If this indices array 
+     * is empty, the internal board array is [reset()]{@link Board#reset} and the function 
+     * will retry. Then, it will pull a random location in the indices array.
+     * 
+     * The inside of the Board class uses an array for the robots which is one dimensional.
+     * This array has equal columns and rows. To make the visual code representation match 
+     * the graphical display, the index is built by adding the y value, flipped, and 
+     * multiplied by 5, adding the x value to return the accurate array index.
+     * 
+     * @returns {number} integer board index of random position without robot
+     */
+    randomEmptyPos() {
         const indices = [];
+        // store only empty entries in indices
         for (let i = 0; i < this.array.length; i++) {
             if (this.array[i] > 0) {
                 continue;
             }
             indices.push(i);
         }
+
+        // if there are no empty indices, reset and retry (recursively)
         if (indices.length < 1) {
             this.reset();
-            return;
+            return this.randomEmptyPos();
         }
 
-        const index = randomIntZero(indices.length);
+        return indices[randomIntZero(indices.length - 1)];
+    }
 
-        this.setRobotRaw(indices[index], size);
+    /**
+     * Returns generator of two values (in order) a pixel x and pixel y to represent the top 
+     * left of the tile on screen at that tileX and tileY coordinates.
+     *
+     * @param {number} tileX integer x coordinate of tile in board
+     * @param {number} tileY integer y coordinate of tile in board
+     * @generator
+     * @yields {number} two numbers for x and y, in order, for tile's pixel positions
+     */
+    *getPixelPos(tileX, tileY) {
+        const wholeTile = tileWidth + tileSpacing;
+        yield (tileX - 2) * wholeTile + 400 - tileWidth * 0.5;
+        yield ((4 - tileY) - 2) * wholeTile + 400 - tileWidth * 0.5;
+    }
+
+    /**
+     * Returns generator of two values (in order) a tile x and tile y to represent the tile
+     * on screen at that pixelX and pixelY coordinates.
+     *
+     * @param {number} pixelX integer x pixel coordinate of board
+     * @param {number} pixelY integer y pixel coordinate of board
+     * @generator
+     * @yields {number} two numbers for x and y, in order, for board's tile positions
+     */
+    *getTilePos(pixelX, pixelY) {
+        const wholeTile = tileWidth + tileSpacing;
+        const halfSpacing = tileSpacing * 0.5;
+        yield (pixelX + tileWidth * 0.5 - 400 + halfSpacing) / wholeTile + 2;
+        yield -((pixelY + tileWidth * 0.5 - 400 + halfSpacing) / wholeTile - 2) + 1;
     }
     
     /**
@@ -167,9 +226,10 @@ class Board {
                 
                 context.fillStyle = this.getColor(x1, y1);
 
+                let pos = this.getPixelPos(x1, y1);
                 context.fillRect(
-                    (x1 - 2) * 120 + 400 - tileWidth * 0.5, 
-                    ((4 - y1) - 2) * 120 + 400 - tileWidth * 0.5, 
+                    pos.next().value, 
+                    pos.next().value, 
                     tileWidth, tileWidth);
             }
         }
@@ -187,6 +247,12 @@ class Board {
  */
 class Interaction {
 
+    constructor() {
+        const pos = board.randomEmptyPos();
+        this.setPosition(pos);
+        board.setRobotRaw(pos, randomSize());
+    }
+
     /**
      * Sets the position, or the raw index of the location of the robot the player is
      * currently controlling and can move/mix.
@@ -196,22 +262,68 @@ class Interaction {
     setPosition(index) {
         this.position = index;
     }
+    
+    /**
+     * Returns true if the player is controlling a robot, false otherwise.
+     *
+     * @returns {boolean} if the player is controlling a robot.
+     */
+    hasPosition() {
+        return this.position >= 0;
+    }
 
     /**
      * Returns if the currently controlled robot can move into a new position. If no robot is
      * controlled, false, if new position is empty, true, or if the size of the robot in the
-     * new position is equal to the size of the controlled robot, true.
+     * new position is equal to the size of the controlled robot, true. If the new position 
+     * is more than 1 tile away, false.
      * 
      * @param {number} newX integer x coordinate the user's robot is trying to move into
      * @param {number} newY integer y coordinate the user's robot is trying to move into
      * @returns {boolean} if can move into new inputted position
      */
     canMove(newX, newY) {
-        if (this.position < 0) return false;
+        const newPosition = board.toRawIndex(newX, newY);
+        return this.getAvailableMoves().includes(newPosition);
+    }
 
-        const newRobot = board.getRobot(newX, newY);
-        return newRobot == 0 || 
-            board.getRobotRaw(this.position) == newRobot;
+    /**
+     * Adds new position to array if the new position is valid. A new position is valid if
+     * it is an index inside the board array (board.array.length > x > 0)
+     * 
+     * @param {number} array 
+     * @param {number} newPosition 
+     */
+    addMoveIfPossible(array, newPosition) {
+        if (newPosition < 0) return;
+        if (newPosition >= board.array.length) return;
+
+        const newRobot = board.getRobotRaw(newPosition);
+        if (newRobot != 0 && 
+            board.getRobotRaw(this.position) != newRobot) {
+            return;
+        }
+        
+        array.push(newPosition);
+    }
+
+    getAvailableMoves() {
+        if (!this.hasPosition()) return Array();
+
+        const robot = board.getRobotRaw(this.position);
+
+        const availableMoves = Array();
+
+        this.addMoveIfPossible(availableMoves, this.position - 5 - 1);
+        this.addMoveIfPossible(availableMoves, this.position - 5);
+        this.addMoveIfPossible(availableMoves, this.position - 5 + 1);
+        this.addMoveIfPossible(availableMoves, this.position - 1);
+        this.addMoveIfPossible(availableMoves, this.position + 1);
+        this.addMoveIfPossible(availableMoves, this.position + 5 - 1);
+        this.addMoveIfPossible(availableMoves, this.position + 5);
+        this.addMoveIfPossible(availableMoves, this.position + 5 + 1);
+
+        return availableMoves;
     }
 
     /**
@@ -223,7 +335,6 @@ class Interaction {
     delete() {
         const oldSize = board.getRobotRaw(this.position);
         board.setRobotRaw(this.position, 0);
-        this.position = -1;
         return oldSize;
     }
 
@@ -236,7 +347,16 @@ class Interaction {
      * @param {number} newY integer y coordinate the user's robot is trying to move into
      */
     mix(newX, newY) {
-        board.setRobot(newX, newY, this.delete() + 1);
+        let newSize = this.delete() + 1;
+        const newPos = board.toRawIndex(newX, newY);
+        if (newSize >= robotToColor.length) {
+            newSize = 0;
+            this.position = -1;
+        }
+        else {
+            this.position = newPos;
+        }
+        board.setRobotRaw(newPos, newSize);
     }
 
     /**
@@ -250,6 +370,7 @@ class Interaction {
      */
     move(newX, newY) {
         board.setRobot(newX, newY, this.delete());
+        this.position = -1;
     }
 
 }
@@ -257,10 +378,9 @@ class Interaction {
 // constants
 /**
  * The pixel width and height of every tile in drawing on the board
- *
- * @type {number}
  */
 const tileWidth = 100;
+const tileSpacing = 20;
 /**
  * The intentionally unchanged array that takes the robot's size to its color
  *
@@ -273,6 +393,29 @@ const robotToColor = ["black", "red", "green", "blue", "purple"];
  * @type {HTMLCanvasElement}
  */
 const canvas = document.getElementById("gameCanvas");
+canvas.addEventListener('click', function(event) {
+    const pos = board.getTilePos(event.offsetX, event.offsetY);
+    let posX = Math.floor(pos.next().value);
+    let posY = Math.floor(pos.next().value);
+    
+    // move current robot
+    if (!interaction.canMove(posX, posY)) {
+        return;
+    }
+    
+    if (board.getRobot(posX, posY) > 0) {
+        interaction.mix(posX, posY);
+    }
+    else {
+        interaction.move(posX, posY);
+    }
+    
+    if (interaction.position > -1) return;
+    // new robot
+    const newPos = board.randomEmptyPos();
+    interaction.setPosition(newPos);
+    board.setRobotRaw(newPos, randomSize());
+});
 /**
  * The 2d rendering context used for drawing the raw shapes and displays on
  * screen throughout the script 
@@ -318,13 +461,16 @@ function randomIntZero(end) {
     return randomInt(0, end);
 }
 
+function randomSize() {
+    return randomInt(1, 4);
+}
+
 // cycle and draw
 /**
  * The function that draws and updates the game called on every canvas cycle
  */
 function cycle() {
     context.clearRect(0, 0, canvas.width, canvas.height);
-    board.randomRobot(randomInt(1, 4));
     draw();
 }
 
@@ -337,4 +483,4 @@ function draw() {
 
 // init
 draw();
-setInterval(cycle, 1000); // 1000 ms
+setInterval(cycle, 0); // 1000 ms
